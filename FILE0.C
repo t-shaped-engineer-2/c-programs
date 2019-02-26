15db2cc11415d54593a444648aa8ed4e5f25076a
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-02-26 21:04:21 +0530

    Add files via upload

---
+++ FILE0.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	FILE *fp;
+	char ch;
+	clrscr();
+	fp=fopen("filetxt.c","r");
+	ch=getc(fp);
+	while(ch!=EOF)
+	{
+		putchar(ch);
+		ch = getc(fp);
+	}
+	getch();
+
+}
+
